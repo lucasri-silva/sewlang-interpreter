a6e0214573d0c2a446a929e8d6d8f0ea5de64842
Lucas <lucasrsilva.dev@gmail.com>
2022-02-09 15:27:34 -0300

    Update Exp.scala

--- src/main/scala/sewlang/ast/Exp.scala
+++ src/main/scala/sewlang/ast/Exp.scala
@@ -14,16 +14,23 @@ case class IdExp(id: String) extends Exp
 case class SumExp(exp1: Exp, exp2: Exp) extends Exp // (+ exp1 exp2)
 case class MultExp(exp1: Exp, exp2: Exp) extends Exp // (* exp1 exp2)
 // #1 Defina a AST para as expressões (- exp1 exp2), (/ exp1 exp2) e (- exp)
+case class SubtExp(exp1: Exp, exp2: Exp) extends Exp // (- exp1 exp2)
+case class DivExp(exp1: Exp, exp2: Exp) extends Exp // (/ exp1 exp2)
+case class NegExp(exp: Exp) extends Exp // (- exp)
 
 // Relation Expressions
 case class EqualExp(exp1: Exp, exp2: Exp) extends Exp // (= exp1 exp2)
 case class LessThanExp(exp1: Exp, exp2: Exp) extends Exp // (< exp1 exp2)
 // #2 Defina a AST para as expressões (<= exp1 exp2), (> exp1 exp2) e (>= exp1 exp2)
+case class LessEqualExp(exp1: Exp, exp2: Exp) extends Exp // (<= exp1 exp2)
+case class GreaterThanExp(exp1: Exp, exp2: Exp) extends Exp // (> exp1 exp2)
+case class GreaterEqualExp(exp1: Exp, exp2: Exp) extends Exp // (>= exp1 exp2)
 
 // Boolean Expressions
 case class NotExp(exp: Exp) extends Exp // (not exp)
 case class AndExp(exp1: Exp, exp2: Exp) extends Exp // (and exp1 exp2)
 // #3 Defina a AST para a expressão (or exp1 exp2)
+case class OrExp(exp1: Exp, exp2: Exp) extends Exp // (or exp1 exp2)
 
 // Variable Declaration and Assignment Expressions
 case class VarDeclExp(id: Exp, exp: Exp) extends Exp // (var id exp)
@@ -42,3 +49,5 @@ case class BlockExp(exps: List[Exp]) extends Exp // (begin exp*)
 case class PrintExp(exps: List[Exp]) extends Exp // (print exp*)
 case object ReadNumExp extends Exp // (read-num)
 // #4 Defina a AST para as expressões (read-bool) e (read-str)
+case object ReadBoolExp extends Exp // (read-bool)
+case object ReadStrExp extends Exp // (read-str)
