16516d4e1f60b10c13d0bcd35ca50b2a971ab119
Lucas <lucasrsilva.dev@gmail.com>
2022-02-09 14:57:37 -0300

    Update ExpDesugar.scala

--- src/main/scala/sewlang/parser/ExpDesugar.scala
+++ src/main/scala/sewlang/parser/ExpDesugar.scala
@@ -10,9 +10,13 @@ object ExpDesugar {
 
     case SList(List(SSym("++"), id))      => SList(List(SSym("set"), id, SList(List(SSym("+"), id, SNum(1))))) // (++ id) --> (set id (+ id 1))
     // #14 Implemente o desugar para a expressão (-- id)
+    case SList(List(SSym("--"), id))      => SList(List(SSym("set"), id, SList(List(SSym("-"), id, SNum(1))))) // (-- id) --> (set id (- id 1))
 
     case SList(List(SSym("+="), id, exp)) => SList(List(SSym("set"), id, SList(List(SSym("+"), id, exp)))) // (+= id exp) --> (set id (+ id exp))
     // #15 Implemente o desugar para as expressões (*= id exp), (-= id exp) e (/= id exp)
+    case SList(List(SSym("*="), id, exp)) => SList(List(SSym("set"), id, SList(List(SSym("*"), id, exp)))) // (*= id exp) --> (set id (* id exp))
+    case SList(List(SSym("-="), id, exp)) => SList(List(SSym("set"), id, SList(List(SSym("-"), id, exp)))) // (-= id exp) --> (set id (- id exp))
+    case SList(List(SSym("/="), id, exp)) => SList(List(SSym("set"), id, SList(List(SSym("/"), id, exp)))) // (/= id exp) --> (set id (/ id exp))
 
     case SList(List(SSym("for"), init, cond, mod, body)) => // (for init cond mod body) --> (begin init (while cond (begin body mod)))
       SList(List(
@@ -26,6 +30,16 @@ object ExpDesugar {
             mod))))))
 
     // #16 Implemente o desugar para a expressão (repeat body until-cond)
+    // case SList(List(SSym("repeat"), body, untilCond)) =>
+    //   SList(List(
+    //     SSym("begin"),
+    //     body,
+    //     SList(List(
+    //       SSym("repeat"),
+    //       SList(List(
+    //         SSym("begin"),
+    //         body,
+    //         cond))))))
 
     case _ => sexp
   }
